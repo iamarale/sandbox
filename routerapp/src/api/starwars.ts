9f0f3603fddf0e7cbf37349f056a9883d71ffc7d
iamarale <ismailarales@gmail.com>
2024-10-01 08:47:18 -0500

    delete app

--- routerapp/src/api/starwars.ts
+++
@@ -1,5 +0,0 @@
-export async function getStarwarsData(category: string, id: string) {
-  const res = await fetch(`https://swapi.dev/api/${category}/${id}`);
-  const data = await res.json();
-  return data;
-}
