1eea3caeb6e8e363e0630e71840899d4a32178bc
iamarale <ismailarales@gmail.com>
2024-10-03 03:05:32 -0500

    displayed all countries and fixed img height

--- countries-app/src/App.tsx
+++ countries-app/src/App.tsx
@@ -1,3 +1,34 @@
+import { useEffect, useState } from "react";
+import AllCountries from "./components/allCountries";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [data, setData] = useState<Array>();
+  const [error, setError] = useState<string>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  //   Api Call
+  useEffect(() => {
+    async function getAllCountries() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`https://restcountries.com/v3.1/all`);
+        const resData = await res.json();
+        setData(resData);
+        console.log(data);
+        setIsLoading(false);
+      } catch (e) {
+        setError(e);
+      }
+    }
+    getAllCountries();
+  }, []);
+
+  if (isLoading) return <h1>Loading</h1>;
+  if (error) return <h1>Error.. {error}</h1>;
+
+  return (
+    <main>
+      <AllCountries countriesData={data} />
+    </main>
+  );
 }
--- countries-app/src/routes/__root.tsx
+++ countries-app/src/routes/__root.tsx
@@ -1,19 +1,16 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import Header from "../components/Header";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
+      <div className="h-full bg-zinc-900 text-zinc-300">
+        <Header />
+        <div className="container mx-auto p-2">
+          <Outlet />
+        </div>
       </div>
-      <hr />
-      <Outlet />
       <TanStackRouterDevtools />
     </>
   ),
